1a2a816d5fd4abe8f099d49a856d3dc67f196e1b
Simon Bates <simon@bitstructures.com>
2019-02-20 13:26:18 -0500

    C2LC-1: Add License

--- examples/hello.js
+++ examples/hello.js
@@ -1,3 +1,13 @@
+/*
+Coding to Learn and Create
+Copyright 2019 OCAD University
+
+Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
+
+You may obtain a copy of the 3-Clause BSD License at
+https://github.com/simonbates/c2lc-exploratory/raw/master/LICENSE.txt
+*/
+
 /* eslint-env node */
 
 "use strict";
