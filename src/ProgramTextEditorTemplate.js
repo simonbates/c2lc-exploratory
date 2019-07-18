2990d536e040595cb6f9071823507b471af65f63
Simon Bates <simon@bitstructures.com>
2019-07-18 16:24:27 -0400

    C2LC-11: Move text editor instructions to template

--- src/ProgramTextEditorTemplate.js
+++ src/ProgramTextEditorTemplate.js
@@ -10,6 +10,8 @@ https://github.com/simonbates/c2lc-exploratory/raw/master/LICENSE.txt
 
 /* global Handlebars */
 
+// TODO: Put the template into a "handlebars" file and precompile
+
 (function () {
 
     "use strict";
@@ -20,7 +22,13 @@ https://github.com/simonbates/c2lc-exploratory/raw/master/LICENSE.txt
         "<label class='c2lc-programTextEditor-label'" +
         "    for='{{textareaId}}'>Program:</label>" +
         "<textarea class='c2lc-programTextEditor-textarea'" +
-        "    id='{{textareaId}}' cols='40' rows='4'></textarea>"
+        "    id='{{textareaId}}' cols='40' rows='4'></textarea>" +
+        "<div class='c2lc-programTextEditor-instructions'>" +
+        "    Available actions: forward, left, right.<br>" +
+        "    Write your program actions in the box above and separate them " +
+        "    by spaces. For example, to draw a square, use: " +
+        "    forward left forward left forward left forward left" +
+        "</div>"
     );
 
 })();
